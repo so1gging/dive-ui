5ec932cbb033eb52aca261cd9125f58778ae2933
김솔지 <so1gging@gmail.com>
2023-12-18 18:45:57 +0900

    :wastebasket:

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,5 +20,6 @@ module.exports = {
       allowConstantExport: true
     }],
     '@typescript-eslint/no-non-null-assertion': 'off'
-  }
+  },
+  ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <></>
+  return <div className="flex"></div>
 }
 
 export default App
